d4123085058da9803c2dbd11914110fcf85b2a42
Léo Millon <millon.leo@gmail.com>
2015-03-28 18:12:34 +0100

    Remove unused import

--- src/main/java/com/github/leomillon/properties/controller/PropertiesAnalyzerController.java
+++ src/main/java/com/github/leomillon/properties/controller/PropertiesAnalyzerController.java
@@ -18,7 +18,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
